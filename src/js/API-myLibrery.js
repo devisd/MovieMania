abb903e0ff296cc8bc3aad62f38f19784f8275ba
devisd <89788799+devisd@users.noreply.github.com>
2022-05-31 17:42:23 +0700

    fix header, lib, btn and localstorage

--- src/js/API-myLibrery.js
+++ src/js/API-myLibrery.js
@@ -4,33 +4,32 @@ const movies = document.querySelector('.movies');
 const watched = document.querySelector('#watched');
 const queue = document.querySelector('#queue');
 
-const x = [];
-const firstObj = { title: 'ddd', autor: 'avgs', img: 'https://www.mirf.ru/wp-content/uploads/2020/12/Chaos-Walking-1024x683.jpg' };
-const secondObj = { title: 'tttt', autor: 'fndnd', img: 'https://images.alphacoders.com/115/1159315.jpg' };
+const movieStorage = [];
+const firstObj = { title: 'MONSTER HUNTER', genres: 'Drama, Action', year: '2020', rating: '8.0', img: 'https://www.mirf.ru/wp-content/uploads/2020/12/Chaos-Walking-1024x683.jpg' };
+const secondObj = { title: 'MONSTER HUNTER', genres: 'Drama, Action', year: '2020', rating: '8.0', img: 'https://www.mirf.ru/wp-content/uploads/2020/12/Chaos-Walking-1024x683.jpg' };
 
 watched.addEventListener('click', addMovie);
 myLibrary.addEventListener('click', renderMovies);
 
 function addMovie() {
-    x.push(firstObj);
-    console.log(x);
-    localStorage.setItem('my-data', JSON.stringify(x));
+    movieStorage.push(firstObj);
+    localStorage.setItem('my-data', JSON.stringify(movieStorage));
 };
 
-// movies.innerHTML = localStorage.getItem('my-data');
-
 function renderMovies() {
+
     const moviesArray = localStorage.getItem('my-data');
     const parsMovieArray = JSON.parse(moviesArray);
-    console.log(parsMovieArray);
+
     const render = parsMovieArray.map(elem => {
-        return `
-        <div class="movie__cart">
-            <img src="${elem.img}" width=360>
-            <h2>${elem.title}</h2>
-            <p>${elem.autor}</p>
-        </div>
-        `
+        return `<li class="movie__cart">
+                    <img class="movie__poster" src="${elem.img}">
+                    <h2 class="movie__title">${elem.title}</h2>
+                    <div class="movie__text-container">
+                        <p class="movie__description">${elem.genres} | ${elem.year}</p>
+                        <p class="movie__rating">${elem.rating}</p>
+                    </div>
+                </li>`
     }).join('');
-    movies.innerHTML = render;
+    movies.insertAdjacentHTML('beforeend',render);
 };
--- src/js/header.js
+++ src/js/header.js
@@ -2,10 +2,20 @@ const refs = {
     headerBG: document.querySelector('.header'),
     headerBtn: document.querySelector('.lybrary'),
     headerForm: document.querySelector('.header__form'),
+    homeBtn: document.querySelector('#home'),
     lybraryBtn: document.querySelector('#my_library'),
 }
 
+refs.homeBtn.addEventListener('click', onOpenHome);
 refs.lybraryBtn.addEventListener('click', onOpenLybrary);
+
+function onOpenHome(e) {
+    e.preventDefault()
+
+    refs.headerBG.classList.remove("lib");
+    refs.headerForm.style.display = "block";
+    refs.headerBtn.style.display = "none";
+}
 
 function onOpenLybrary(e) {
     e.preventDefault()
