780568c7eed71ea26314b450cb6696f95c7ebdfb
Alfat1989 <gilmalf89@gmail.com>
2022-06-03 17:59:16 +0300

    add event on click to movie card

--- src/js/API-word.js
+++ src/js/API-word.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const URL = 'https://api.themoviedb.org/3/search/movie';
+
 const KEY = 'cae3be8cdf5c9c2351403518efd2a643';
 
 export default class ApiServise{
     constructor() {
+        this.id_movie = '';
         this.page = 1;
         this.searchQuary = '';
     }
@@ -26,5 +28,24 @@ export default class ApiServise{
 
     set query(newQuery) {
         this.searchQuary = newQuery;
+    }
+
+    onIdFetch() {
+        const idM=this.id_movie
+        const ID_URL = `https://api.themoviedb.org/3/movie/${idM}`
+        return axios(ID_URL, {
+            params: {
+                movie_id:this.id_movie,
+                api_key: KEY,
+            }
+        }).then(res=>res.data)
+    }
+
+    get movieId() {
+        return this.id_movie
+    }
+
+    set movieId(newId) {
+        this.id_movie = newId;
     }
 }
