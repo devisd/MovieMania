cb6366c2ddbe69b8a9cd05db6816f6628e565a11
Alfat1989 <gilmalf89@gmail.com>
2022-06-05 21:13:03 +0300

    add markap to movie card when clicked for it

--- src/js/input-fetch.js
+++ src/js/input-fetch.js
@@ -1,8 +1,4 @@
 import API from './API-word'
-// import cardTpl from '../templates/card.hbs'
-// import modalHbs from '../templates/modal.hbs'
-import debounce from 'lodash.debounce'
-
 
 const searchFormEl = document.querySelector('form')
 const galleryDivEl = document.querySelector('.movies')
@@ -10,11 +6,7 @@ const modalPage = document.querySelector("[data-modal]")
 const modalEl = document.querySelector('.modal__box')
 const getApiServise = new API();
 
-// const mainEl=document.querySelector('.main')
-
 searchFormEl.addEventListener('submit', onInput)
-
-
 
 function onInput(e) {
     
@@ -24,7 +16,6 @@ function onInput(e) {
     const searchQuey=e.currentTarget.elements[0].value
 
     movieSearch(searchQuey)
-    // console.log(searchQuey)
 }
 
 function movieSearch(searchQuery) {
@@ -35,10 +26,7 @@ function movieSearch(searchQuery) {
             return data.results
         })
         .then(createMarkapCard)
-        .then(renderMarkap)
-    //     // .catch(onError)
-
-    
+        .then(renderMarkap)    
 }
 
 function createMarkapCard(results) {
@@ -85,46 +73,53 @@ function onCardClick(e) {
         .then(renderModal)    
 }
 
-function openModal({backdrop_path, title, overview, genres, original_title, popularity, vote_average,}) {
+function openModal({backdrop_path, title, vote_count, overview, genres, original_title, popularity, vote_average,}) {
     return `
-        <div class="current__content">
-            <img class="current_image" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="#" />
-        </div>
+      <div class="current__content">
+        <img class="current_image" src="https://image.tmdb.org/t/p/w500${backdrop_path}" alt="#" />
+      </div>
 
-        <!-- правая сторона -->
-        <div class="modal-rigth-side">
+      <!-- правая сторона -->
+      <div class="modal-rigth-side">
         <h3 class="modal-movie-name">${title}</h3>
         <!-- жанры и прочее -->
         <div class="modal-ul-wrapper">
-            <ul class="modal-ul">
-            <li>${vote_average}</li>
+          <ul class="modal-ul modal-ul-sec gray">
+            <li>Vote / Votes</li>
+            <li>Popularity</li>
+            <li>Original Title</li>
+            <li>Genre</li>
+          </ul>
+          <ul class="modal-ul-sec">
+            <li class="gray">
+              <span class="modal-vote">${vote_average}</span> /
+              <span class="modal-votes">${vote_count}</span>
+            </li>
             <li>${popularity}</li>
             <li>${original_title}</li>
-            <li>${genres}</li>
-            </ul>
-            <ul>
-            <li>
-                <span class="modal-vote">BLA</span> /
-                <span class="modal-votes">BLA</span>
-            </li>
-            <li>BLABLABLA</li>
-            <li>BLABLABLA</li>
-            <li>BLABLABLA</li>
-            </ul>
+            <li>${genres.map(res=>res.name)}</li>
+          </ul>
         </div>
         <!-- текст о -->
-        <h4>About</h4>
-        <p>
-            ${overview}
+        <h4 class="modal-about">ABOUT</h4>
+        <p class="modal-text">
+        ${overview}
         </p>
         <!-- кнопки -->
-        <button class="modal-button modal-btn-orange" type="submit" data-button-del-watched>
+        <div class="modal-button-wrapper">
+          <button
+            class="modal-button modal-btn-orange"
+            type="submit"
+            data-button-del-watched
+          >
             ADD TO WATCHED
-        </button>
-        <button class="modal-button modal-btn-orange" type="submit" data-button-del-queue>
+          </button>
+          <button class="modal-button" type="submit" data-button-del-queue>
             ADD TO QUEUE
-        </button>
+          </button>
         </div>
+      </div>
+
     `
 }
 
