32ee870dbf0c757995e099798fad3ed52c34b4fe
devisd <89788799+devisd@users.noreply.github.com>
2022-06-23 08:48:32 +0700

    1

--- src/js/small-modal.js
+++ src/js/small-modal.js
@@ -1,68 +1,64 @@
-
-const modalLink = document.querySelector('.open-small-modal')
-const containerSmall = document.querySelector(".container-small-modal");
-const closeBtn = document.querySelector('.close-small-modal-btn')
-const why = document.querySelector('.why')
-modalLink.after(containerSmall)
-modalLink.addEventListener('click', toggleSmallModal)
+const modalLink = document.querySelector('.open-small-modal');
+const containerSmall = document.querySelector('.container-small-modal');
+const closeBtn = document.querySelector('.close-small-modal-btn');
+const why = document.querySelector('.why');
+modalLink.after(containerSmall);
+modalLink.addEventListener('click', toggleSmallModal);
 function toggleSmallModal() {
   if (containerSmall.classList.contains('small-is-hidden')) {
-    containerSmall.classList.remove('small-is-hidden')
+    containerSmall.classList.remove('small-is-hidden');
   } else {
-    containerSmall.classList.add('small-is-hidden')
+    containerSmall.classList.add('small-is-hidden');
   }
 }
-window.addEventListener('keydown', closeSmallModal)
+window.addEventListener('keydown', closeSmallModal);
 
 function closeSmallModal(e) {
   if (e.code === 'Escape') {
-    containerSmall.classList.add('small-is-hidden')
+    containerSmall.classList.add('small-is-hidden');
   }
 }
 
-closeBtn.addEventListener('click', onCloseBtn)
+closeBtn.addEventListener('click', onCloseBtn);
 
 function onCloseBtn() {
-  containerSmall.classList.add('small-is-hidden')
+  containerSmall.classList.add('small-is-hidden');
 }
 
+const uppBtn = document.querySelector('.up-button');
 
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
 
-const uppBtn = document.querySelector(".up-button");
-
-const downBtn = document.querySelector(".down-button");
-const sidebar = document.querySelector(".sidebar");
-const mainSlide = document.querySelector(".main-slide");
-
-
-const slidesCount = mainSlide.querySelectorAll(".small-div").length;
+const slidesCount = mainSlide.querySelectorAll('.small-div').length;
 console.log(slidesCount);
 let activeSlideIndex = 0;
 
 sidebar.style.top = `-${(slidesCount - 1) * 400}px`;
 
-uppBtn.addEventListener("click", () => {
-  changeSlide("up");
+uppBtn.addEventListener('click', () => {
+  changeSlide('up');
 });
-downBtn.addEventListener("click", () => {
-  changeSlide("down");
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
 });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowUp") {
-    changeSlide("up");
-  } else if (event.key === "ArrowDown") {
-    changeSlide("down");
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (event.key === 'ArrowDown') {
+    changeSlide('down');
   }
 });
 
 function changeSlide(direction) {
-  if (direction === "up") {
+  if (direction === 'up') {
     activeSlideIndex++;
     if (activeSlideIndex === slidesCount) {
       activeSlideIndex = 0;
     }
-  } else if (direction === "down") {
+  } else if (direction === 'down') {
     activeSlideIndex--;
     if (activeSlideIndex < 0) {
       activeSlideIndex = slidesCount - 1;
